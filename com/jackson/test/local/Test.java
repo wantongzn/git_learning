6d1014584fd3972c32b1363c7531d816f1c0c563
wantongzn <zdn.2007@163.com>
2021-10-04 23:48:15 +0800

    add interface and local package

---
+++ com/jackson/test/local/Test.java
@@ -0,0 +1,12 @@
+package t;
+
+/**
+ * @Author jackson
+ * @Date 2021/8/19 21:23
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
